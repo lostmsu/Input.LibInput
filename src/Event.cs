15fbfa93b517807267bdd9dd9767c359e5f2a7d9
Victor Nova <lostfreeman@gmail.com>
2024-11-19 06:15:03 +0000

    bits of parsing

--- src/Event.cs
+++ src/Event.cs
@@ -5,4 +5,22 @@ using YamlDotNet.Serialization;
 public class Event {
     [YamlMember(Alias = "evdev")]
     public int[][] EvDev { get; set; }
+
+    [YamlIgnore]
+    public EvDevEvent[]? EvDevEvents => this.EvDev?.Select(EvDevEvent.Parse).ToArray();
+
+    public override string ToString()
+        => string.Join("\n", (IEnumerable<EvDevEvent>?)this.EvDevEvents ?? []);
+}
+
+public class EvDevEvent {
+    public TimeSpan Timestamp { get; set; }
+
+    internal static EvDevEvent Parse(int[] @event) {
+        return new() {
+            Timestamp = TimeSpan.FromTicks(checked((long)@event[1] * 10)),
+        };
+    }
+
+    public override string ToString() => $"{this.Timestamp}";
 }
